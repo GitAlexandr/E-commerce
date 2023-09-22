382c6e8857c24d0c51dd62e7ffd68a88d636f9e0
GitAlexandr <sashabushuev03@mail.ru>
2023-09-22 21:32:40 +0300

    fix_error

--- backend/access/authAccess.js
+++ backend/access/authAccess.js
@@ -9,13 +9,16 @@ module.exports = function (req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
-            return res.status(403).json({message: "Пользователь не авторизован"})
+            return res.status(403).json({error: "Forbidden", message: "Пользователь не авторизован"})
         }
         const decodedData = jwt.verify(token, secret)
         req.user = decodedData
         next()
     } catch (e) {
         console.log(e)
-        return res.status(403).json({message: "Пользователь не авторизован"})
+        return res.status(403).json({
+            error: "Forbidden",
+            message: "Пользователь не авторизован",
+            details: "Ошибка при проверке токена"})
     }
 };
--- backend/auth/authController.js
+++ backend/auth/authController.js
@@ -18,21 +18,22 @@ class authController {
         try {
             const errors = validationResult(req)
             if (!errors.isEmpty()) {
-                return res.status(400).json({message: "Ошибка при регистрации", errors})
+                return res.status(400).json({ error: 'Bad Request', message: "Ошибка при регистрации", details: errors.array()})
             }
             const {username, password} = req.body;
             const candidate = await User.findOne({username})
             if (candidate) {
-                return res.status(400).json({message: "Пользователь с таким именем уже существует"})
+                return res.status(409).json({ error: 'Conflict', message: "Пользователь с таким именем уже существует"})
             }
             const hashPassword = bcrypt.hashSync(password, 7);
             const userRole = await Role.findOne({value: "USER"})
             const user = new User({username, password: hashPassword, roles: [userRole.value]})
             await user.save()
-            return res.json({message: "Пользователь успешно зарегистрирован"})
+            return res.status(201).json({ message: "Пользователь успешно зарегистрирован" });
+
         } catch (e) {
             console.log(e)
-            res.status(400).json({message: 'Registration error'})
+            res.status(500).json({ error: 'Internal Server Error', message: 'Registration error'})
         }
     }
 
@@ -41,17 +42,17 @@ class authController {
             const {username, password} = req.body
             const user = await User.findOne({username})
             if (!user) {
-                return res.status(400).json({message: `Пользователь ${username} не найден`})
+                return res.status(404).json({error: 'Not Found', message: `Пользователь ${username} не найден`})
             }
             const validPassword = bcrypt.compareSync(password, user.password)
             if (!validPassword) {
-                return res.status(400).json({message: `Введен неверный пароль`})
+                return res.status(401).json({error: 'Unauthorized', message: `Введен неверный пароль`})
             }
             const token = generateAccessToken(user._id, user.roles)
-            return res.json({token})
+            return res.status(200).json({token})
         } catch (e) {
             console.log(e)
-            res.status(400).json({message: 'Login error'})
+            res.status(500).json({error: 'Internal Server Error', message: 'Login error'})
         }
     }
 
